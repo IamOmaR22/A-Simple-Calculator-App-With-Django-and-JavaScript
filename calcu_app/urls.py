7d6a3ab407c3103580eab3b4937c56d520fbcc13
Md. Omar Faruk <iamomar022@gmail.com>
2021-01-16 22:04:30 +0600

    Contact Page

--- calcu_app/urls.py
+++ calcu_app/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
+    path('contact', views.about, name='contact'),
 ]
--- calcu_app/views.py
+++ calcu_app/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'calcu_app/index.html')
 
-
 def about(request):
     return render(request, 'calcu_app/about.html')
+
+def contact(request):
+    return render(request, 'calcu_app/contact.html')
