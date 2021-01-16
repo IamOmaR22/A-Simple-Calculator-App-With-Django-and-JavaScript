392d6d251a5d3a7836d7827064dee74f7c26eed5
Md. Omar Faruk <iamomar022@gmail.com>
2021-01-16 22:06:50 +0600

    Contact Page

--- calcu_app/urls.py
+++ calcu_app/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
-    path('contact', views.about, name='contact'),
+    path('contact', views.contact, name='contact'),
 ]
