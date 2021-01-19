4dbafe94d9c4ed73e31242ac7c879dcecac938d0
Md. Omar Faruk <iamomar022@gmail.com>
2021-01-19 20:24:22 +0600

    contact model

--- calcu_app/models.py
+++ calcu_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    sno = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=13)
+    content = models.TextField()
+    timeStamp = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return 'Message from ' + self.name + ' - ' + self.email
