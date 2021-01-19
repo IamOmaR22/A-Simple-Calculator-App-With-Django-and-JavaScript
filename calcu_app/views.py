8d706d3589a2f829469175ac881c97a649ee8f2c
Md. Omar Faruk <iamomar022@gmail.com>
2021-01-19 20:27:57 +0600

    Contact Views

--- calcu_app/views.py
+++ calcu_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Contact
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,4 +11,16 @@ def about(request):
     return render(request, 'calcu_app/about.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        content = request.POST['content']
+
+        if len(name)<2 or len(email)<3 or len(phone)<10 or len(content)<4:
+            messages.error(request, "Please fill the form correctly!")
+        else:
+            contact = Contact(name=name, email=email, phone=phone, content=content)
+            contact.save()
+            messages.success(request, 'Your message has been sent successfully!')
     return render(request, 'calcu_app/contact.html')
