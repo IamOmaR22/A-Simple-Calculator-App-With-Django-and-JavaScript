6412d1e4ecace9d167ef349b46f473a095eac414
Md. Omar Faruk <iamomar022@gmail.com>
2021-01-19 20:41:04 +0600

    Contact template

--- calculator/settings.py
+++ calculator/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')   ## location/address of the static folder
 ]
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
